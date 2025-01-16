256268a03f5208aae8b43de89d97e0f17b844e72
nischalshakya <107358076+nischalshakya787@users.noreply.github.com>
2025-01-16 12:20:04 +0545

    fix: removed unused component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import { Box, Typography, Button } from '@mui/material';
-import BackgroundAnimation from './components/ParticleAnimation/BackgroundAnimation';
-import Link from 'next/link';
+import React from "react";
+import { Box, Typography, Button } from "@mui/material";
+import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <Box sx={{ position: 'relative', zIndex: 1, textAlign: 'center', marginTop: '20%' }}>
+    <Box
+      sx={{
+        position: "relative",
+        zIndex: 1,
+        textAlign: "center",
+        marginTop: "20%",
+      }}
+    >
       <Typography variant="h1">404</Typography>
       <Typography variant="h5">Page Not Found</Typography>
       <Link href="/" passHref>
-        <Button variant="contained" color="primary" sx={{ marginTop: '20px' }}>
+        <Button variant="contained" color="primary" sx={{ marginTop: "20px" }}>
           Go to Home
         </Button>
       </Link>
