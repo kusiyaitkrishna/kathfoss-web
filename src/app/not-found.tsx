0e3c4c9aa7b875496109c3818d44c5b2c569b835
nischalshakya <107358076+nischalshakya787@users.noreply.github.com>
2025-01-16 10:42:55 +0545

    Update src/app/not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,6 @@ import Link from 'next/link';
 const NotFound = () => {
   return (
     <Box sx={{ position: 'relative', zIndex: 1, textAlign: 'center', marginTop: '20%' }}>
-      <BackgroundAnimation />
       <Typography variant="h1">404</Typography>
       <Typography variant="h5">Page Not Found</Typography>
       <Link href="/" passHref>
